b2ebb660ae02e156562d7098d0632fae88bcfe79
Matt Butler <mattb-w@hotmail.co.uk>
2016-06-06 15:54:46 +0100

    added basic html/css

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-
+set :session_secret, 'super secret'
 
 get '/' do
   "Hello World"
@@ -10,7 +10,9 @@ get '/secret' do
 end
 
 get '/cat' do
-  "meow"
+  "<div style='border: 3px dashed red'>
+  <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>
+  </div>"
 end
 
 get '/cow' do
