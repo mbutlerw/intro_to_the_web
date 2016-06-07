74db6ebd24eb3f57dcff84f73774254303b83c64
Andy <andy.bell.github@gmail.com>
2016-06-07 10:36:59 +0100

    created views folder, moved cat form functionality to new page

--- app.rb
+++ app.rb
@@ -9,10 +9,16 @@ get '/secret' do
   "This is a secret"
 end
 
-get '/cat' do
-  "
-    <div style='border: 3px dashed red'>
-      <img src='http://bit.ly/1eze8aE'
-    </div>
-  "
+get '/random-cat' do
+  @name = ["Amigo","Oscar","Viking"].sample
+  erb(:index)
+end
+
+post '/named-cat' do
+  @name = params[:name]
+  erb(:index)
+end
+
+get '/cat-form' do
+  erb(:cat_form)
 end
